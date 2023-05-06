58502ab1f8c7f013c24d3462f4a26583537d0344
K-JC <kellytolfts93@hotmail.com>
2023-05-06 14:05:28 +0000

    adding to the models so user can pick time/date

--- bookingsystem/models.py
+++ bookingsystem/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+PICK_TIME = (
+    ('12:00 PM', '12:00 PM'),
+    ('1:00 PM', '1:00 PM'),
+    ('2:00 PM', '2:00 PM'),
+    ('3:00 PM', '3:00 PM'),
+    ('4:00 PM', '4:00 PM'),
+    ('5:00 PM', '5:00 PM'),
+    ('6:00 PM', '6:00 PM'),
+    ('7:00 PM', '7:00 PM'),
+    ('8:00 PM', '8:00 PM'),
+    ('9:00 PM', '9:00 PM'),
+    ('10:00 PM', '10:00 PM'),
+)
