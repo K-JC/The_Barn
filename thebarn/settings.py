05020fd5320a96c3e4ceb7a4d590ab318ed151ea
K-JC <kellytolfts93@hotmail.com>
2023-05-20 18:48:57 +0000

    add allauth apps to settings.py file, and add redirect urls for logging in and logging out of account

--- thebarn/settings.py
+++ thebarn/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -60,6 +63,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'thebarn.urls'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+SITE_ID = 1
+LOGIN_REDIRECT_URL ='/'
+LOGOUT_REDIRECT_URL ='/'
 
 TEMPLATES = [
     {
