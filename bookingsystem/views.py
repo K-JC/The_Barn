ffd2f8586f7663b278daf1e6b2bdbde844009fda
K-JC <kellytolfts93@hotmail.com>
2023-05-20 11:33:01 +0000

    adding content to my base and index.html files, fixed the bug that was causing my text not to show. changed base to index.html in my views folder.Bug fixed

--- bookingsystem/views.py
+++ bookingsystem/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 
 from .models import guest_booking
 
 
-# Restaurant home page class
+# Restaurant home page class, renders the page in the browser
 class Home (generic.ListView):
     model = guest_booking
-    template_name = 'base.html'
+    template_name = 'index.html'
+
+
+# The get request returns the template as above
+def get(self, request):
+    return render(request, 'base.html')
