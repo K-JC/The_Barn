ed8fa19f2b25223f7ab47fb8b65bb7ce8105f087
K-JC <kellytolfts93@hotmail.com>
2023-05-20 18:44:47 +0000

    add path for allauth in urls.py file

--- thebarn/urls.py
+++ thebarn/urls.py
@@ -21,4 +21,5 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bookingsystem.urls'), name='bookingsystem_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
