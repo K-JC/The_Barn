3099497694fbcc2f3fb7578e134352c9b18ec5f7
K-JC <kellytolfts93@hotmail.com>
2023-05-20 08:27:32 +0000

    fix bug so webpage would load, add site to allowed hosts in settings file. Add plan to my index.html file for my content and where I want it to be accoring to my figma plan

--- thebarn/settings.py
+++ thebarn/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['the-barn.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['the-barn.herokuapp.com', 'localhost', '8000-kjc-thebarn-kbsjk8ya556.ws-eu97.gitpod.io']
 
 
 # Application definition
